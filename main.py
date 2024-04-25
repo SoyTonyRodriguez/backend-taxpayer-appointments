526f39f67f0198fdc91db327aff74ad3ba9c57ad
SoyTonyRodriguez <rodriguez.flores.antonioo@gmail.com>
2024-04-25 12:17:11 -0600

    Calculated distance between office and client location

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import json
-
 from haversine import haversine
+
+
+def load_clients(filepath):
+    try:
+        with open(filepath, "r") as file:
+            clients = json.load(file)
+        return clients
+    except FileNotFoundError:
+        print("The file was not found")
+        return []
 
 
 # Main function
@@ -8,9 +17,15 @@ if __name__ == "__main__":
     # The office location, can be changed
     office_location = {"latitude": 19.3797208, "longitude": -99.1940332}
 
-    distance = haversine(
-        (20.28, -103.42),
-        (office_location["latitude"], office_location["longitude"]),
-    )
+    filepath = "./sample-data/taxpayers.json"
+    clients = load_clients(filepath)
 
-    print(distance)
+    for client in clients:
+        client_location = client["location"]
+        distance = haversine(
+            (client_location["latitude"], client_location["longitude"]),
+            (office_location["latitude"], office_location["longitude"]),
+        )
+        print(distance)
+
+    # print(distance)
